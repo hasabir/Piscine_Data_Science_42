4d41c9bd2453ded2da0aba70743787729792d9b7
Hajar Sabir <hasabir@c4r10p3.1337.ma>
2025-01-13 09:30:55 +0100

    modifi ex01

--- Data_Warehouse/ex01/customers_table.py
+++ Data_Warehouse/ex01/customers_table.py
@@ -3,59 +3,24 @@ import os
 import pandas as pd
 import sqlalchemy as sql
 
-type_dict = {
-    'event_time': sql.types.TIMESTAMP(timezone=True),
-    'event_type': sql.types.TEXT,
-    'product_id': sql.types.BIGINT,
-    'price': sql.types.NUMERIC,
-    'user_id': sql.types.BIGINT,
-    'user_session': sql.types.UUID
-}
 
-def create_table():
-    return f"""
-CREATE TABLE IF NOT EXISTS public.customer (
-    event_time TIMESTAMP WITH TIME ZONE,
-    event_type character varying,
-    product_id bigint,
-    price numeric,
-    user_id bigint,
-    user_session uuid
-);"""
 
 def main():
     db = sql.create_engine("postgresql://hasabir:mysecretpassword@localhost:5432/piscineds")
-    
     engine = db.connect()
-    connection = psycopg2.connect(
-        host='localhost',
-        port='5432',
-        database="piscineds",
-        user='hasabir',
-        password='mysecretpassword'
-    )
-
     
-    
-    cursor = connection.cursor()
-    cursor.execute(create_table())
-    connection.commit()
-    
-     
-    customer = pd.DataFrame()
     directory_path = "../subject"
     for root, _, files in os.walk(directory_path):
         current_dir = root.split('/')[-1]
-        for file in files:
-            if current_dir == 'customer':
-                print(f"{root}/{file}")
-                customer = pd.concat([customer, pd.read_csv(f"{root}/{file}")])
-
-    customer.to_sql('customer', engine, if_exists='replace', index=False)
-    connection.commit()
-    connection.close()
+        if current_dir == 'customer':
+            for file in files:
+                file_path = f"{root}/{file}"
+                print(file_path)
+                customers  = pd.read_csv(file_path)
+                # for chunk in pd.read_csv(file_path, chunksize=10000):
+                customers.to_sql('customers', engine, if_exists='append', index=False)
     
-# , dtype=type_dict
+    engine.close()
 
 if __name__ == "__main__":
     main()
