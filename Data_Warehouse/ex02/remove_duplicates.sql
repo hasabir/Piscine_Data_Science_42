3dd74dce7061428c86f2ff7abb8dece830ead78a
Hajar Sabir <hasabir@e1r1p8.1337.ma>
2025-01-14 21:52:06 +0100

    push

--- Data_Warehouse/ex02/remove_duplicates.sql
+++ Data_Warehouse/ex02/remove_duplicates.sql
@@ -1,19 +1,3 @@
--- DELETE FROM customers
--- USING (
---     SELECT ctid, product_id, user_id, event_type, user_session
---     FROM (
---         SELECT ctid, product_id, user_id, event_type, user_session,
---                ROW_NUMBER() OVER (
---                    PARTITION BY product_id, user_id, event_type, user_session
---                    ORDER BY product_id ASC
---                ) AS row_num
---         FROM customers
---     ) AS subquery
---     WHERE row_num > 1
--- ) AS duplicates
--- WHERE customers.ctid = duplicates.ctid;
-
-
 
 WITH CTE AS (
     SELECT 
